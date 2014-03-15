8c7212c24882fdbd750f59b658071bf63c0ea946
Italo Otaviano <italootaviano@gmail.com>
2014-03-15 16:16:22 -0400

    made some progress

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazarCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start () {
+		StartCoroutine(SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves() {
+
+		yield return new WaitForSeconds(startWait);
+
+		while(true){
+			for(int i=0; i < hazarCount; i++){
+				
+				Vector3 spawnPostion = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				
+				Instantiate(hazard, spawnPostion, spawnRotation);	
+				
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+
+		}
+	}
+}
---
+++ Space Shooter/Assets/Scripts/RamdonRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RamdonRotator : MonoBehaviour {
+	public float tumble;
+
+	void Start(){
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;	
+	
+	}
+}
